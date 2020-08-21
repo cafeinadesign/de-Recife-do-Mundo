114833670e5d1f82c191daa02d3f6720085c1c9f
Cafeína Design <cafeinadesign@gmail.com>
2020-08-21 19:00:11 -0300

    paginação do ePub

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import ePub, { Book, Rendition } from 'epubjs';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  title;
+  rendition: Rendition;
+  constructor() {
+    const book: Book = ePub(
+      'assets/robertinho-de-recife-robertinho-do-mundo.epub'
+    );
+    book.loaded.metadata.then((dados) => {
+      this.title = dados.title;
+    });
+    this.rendition = book.renderTo('area', {
+      flow: 'auto',
+      width: '100%',
+      height: '100%',
+    });
+    this.rendition.display();
+  }
+  prev() {
+    this.rendition.prev();
+  }
+  next() {
+    this.rendition.next();
+  }
 }
